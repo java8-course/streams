433bee09a5a40451ccdccb95b3144d88410a1b9e
Sergei Ermushin <sergei_ermushin@epam.com>
2016-09-29 21:49:28 +0300

    HW: Part1 completed. Part2: CollectorsExercise1

--- src/test/java/part2/exercise/CollectorsExercise1.java
+++ src/test/java/part2/exercise/CollectorsExercise1.java
@@ -6,17 +6,11 @@ import data.Person;
 import org.junit.Test;
 
 import java.util.*;
-import java.util.concurrent.ThreadLocalRandom;
-import java.util.function.BiConsumer;
-import java.util.function.BinaryOperator;
-import java.util.function.Function;
-import java.util.function.Supplier;
+import java.util.function.*;
 import java.util.stream.Collector;
-import java.util.stream.Collectors;
-import java.util.stream.IntStream;
 import java.util.stream.Stream;
 
-import static java.util.stream.Collectors.toList;
+import static java.util.stream.Collectors.*;
 
 public class CollectorsExercise1 {
 
@@ -51,18 +45,89 @@ public class CollectorsExercise1 {
         }
     }
 
+    private static Stream<PersonPositionDuration> getPPDStream(List<Employee> employees) {
+        return employees.stream()
+                .flatMap(e -> e.getJobHistory().stream()
+                        .map(
+                                entry -> new PersonPositionDuration(e.getPerson(), entry.getPosition(), entry.getDuration())
+                        )
+                );
+    }
+
     // With the longest duration on single job
     private Map<String, Person> getCoolestByPosition(List<Employee> employees) {
-        // First option
-        // Collectors.maxBy
-        // Collectors.collectingAndThen
-        // Collectors.groupingBy
 
-        // Second option
-        // Collectors.toMap
-        // iterate twice: stream...collect(...).stream()...
-        // TODO
-        throw new UnsupportedOperationException();
+        Map<String, Person> map1 = getPPDStream(employees)
+                .collect(
+                        groupingBy(
+                                PersonPositionDuration::getPosition,
+                                collectingAndThen(
+                                        maxBy(Comparator.comparing(PersonPositionDuration::getDuration)),
+                                        p -> p.get().getPerson()
+                                )
+                        )
+                );
+
+        Map<String, Person> map2 = getPPDStream(employees)
+                .collect(
+                        toMap(
+                                PersonPositionDuration::getPosition,
+                                Function.identity(),
+                                BinaryOperator.maxBy(Comparator.comparing(PersonPositionDuration::getDuration))
+                        )
+                )
+                .entrySet().stream()
+                .collect(
+                        toMap(
+                                Map.Entry::getKey,
+                                entry -> entry.getValue().getPerson()
+                        )
+                );
+
+        //using custom collector
+        return getPPDStream(employees)
+                .collect(new CoolestPositionCollector());
+    }
+
+    private static class CoolestPositionCollector implements Collector<PersonPositionDuration, Map<String, PersonPositionDuration>, Map<String, Person>> {
+
+        private final BinaryOperator<PersonPositionDuration> combiner = BinaryOperator.maxBy(
+                Comparator.comparing(PersonPositionDuration::getDuration));
+
+        @Override
+        public Supplier<Map<String, PersonPositionDuration>> supplier() {
+            return HashMap::new;
+        }
+
+        @Override
+        public BiConsumer<Map<String, PersonPositionDuration>, PersonPositionDuration> accumulator() {
+            return (map, ppd) -> map.merge(ppd.getPosition(), ppd, combiner);
+        }
+
+        @Override
+        public BinaryOperator<Map<String, PersonPositionDuration>> combiner() {
+            return (m1, m2) -> {
+                for (Map.Entry<String, PersonPositionDuration> e : m2.entrySet())
+                    m1.merge(e.getKey(), e.getValue(), combiner);
+                return m1;
+            };
+        }
+
+        @Override
+        public Function<Map<String, PersonPositionDuration>, Map<String, Person>> finisher() {
+            return map -> map.entrySet().stream()
+                    .collect(
+                            toMap(
+                                    Map.Entry::getKey,
+                                    entry -> entry.getValue().getPerson()
+                            )
+                    );
+        }
+
+        @Override
+        public Set<Characteristics> characteristics() {
+            return Collections.emptySet();
+        }
     }
 
     @Test
@@ -75,8 +140,71 @@ public class CollectorsExercise1 {
     // With the longest sum duration on this position
     // { John Doe, [{dev, google, 4}, {dev, epam, 4}] } предпочтительнее, чем { A B, [{dev, google, 6}, {QA, epam, 100}]}
     private Map<String, Person> getCoolestByPosition2(List<Employee> employees) {
-        // TODO
-        throw new UnsupportedOperationException();
+
+        Map<String, Person> map1 = employees.stream()
+                .flatMap(e -> e.getJobHistory().stream()
+                        .collect(
+                                groupingBy(
+                                        JobHistoryEntry::getPosition,
+                                        summingInt(JobHistoryEntry::getDuration)
+                                )
+                        )
+                        .entrySet().stream()
+                        .map(entry -> new PersonPositionDuration(e.getPerson(), entry.getKey(), entry.getValue()))
+                )
+                .collect(
+                        groupingBy(
+                                PersonPositionDuration::getPosition,
+                                collectingAndThen(
+                                        maxBy(Comparator.comparing(PersonPositionDuration::getDuration)),
+                                        p -> p.get().getPerson()
+                                )
+                        )
+                );
+
+        Map<String, Person> map2 = employees.stream()
+                .flatMap(e -> e.getJobHistory().stream()
+                        .collect(new JobHistoryCollector())
+                        .entrySet().stream()
+                        .map(entry -> new PersonPositionDuration(e.getPerson(), entry.getKey(), entry.getValue()))
+                )
+                .collect(new CoolestPositionCollector());
+
+        return map1;
+    }
+
+    private static class JobHistoryCollector implements Collector<JobHistoryEntry, Map<String, Integer>, Map<String, Integer>> {
+
+        private final BiFunction<Integer, Integer, Integer> combiner = Integer::sum;
+
+        @Override
+        public Supplier<Map<String, Integer>> supplier() {
+            return HashMap::new;
+        }
+
+        @Override
+        public BiConsumer<Map<String, Integer>, JobHistoryEntry> accumulator() {
+            return (map, entry) -> map.merge(entry.getPosition(), entry.getDuration(), combiner);
+        }
+
+        @Override
+        public BinaryOperator<Map<String, Integer>> combiner() {
+            return (m1, m2) -> {
+                for (Map.Entry<String, Integer> e : m2.entrySet())
+                    m1.merge(e.getKey(), e.getValue(), combiner);
+                return m1;
+            };
+        }
+
+        @Override
+        public Function<Map<String, Integer>, Map<String, Integer>> finisher() {
+            return Function.identity();
+        }
+
+        @Override
+        public Set<Characteristics> characteristics() {
+            return Collections.unmodifiableSet(EnumSet.of(Collector.Characteristics.IDENTITY_FINISH));
+        }
     }
 
     private List<Employee> getEmployees() {
