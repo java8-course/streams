1f8f4690ba75a69b2612a4259f0bd25a3ba2ef28
Artem Lodygin <artem_lodygin@epam.com>
2016-10-07 17:32:54 +0300

    Set collector w/test

--- src/test/java/bonus/SetCollectorTest.java
+++ src/test/java/bonus/SetCollectorTest.java
@@ -1,31 +1,21 @@
 package bonus;
 
-import org.openjdk.jmh.annotations.*;
+import org.junit.Test;
 
-import java.util.Arrays;
 import java.util.Set;
-import java.util.concurrent.ThreadLocalRandom;
-import java.util.concurrent.TimeUnit;
-import java.util.stream.IntStream;
 
-@Fork(5)
-@BenchmarkMode(Mode.AverageTime)
-@OutputTimeUnit(TimeUnit.MILLISECONDS)
-//@Warmup(iterations = 20, time = 1, timeUnit = TimeUnit.MINUTES)
-//@Measurement(iterations = 20, time = 1, timeUnit = TimeUnit.MINUTES)
-@State(Scope.Thread)
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThat;
+
 public class SetCollectorTest {
-    private Integer[] millionInts = IntStream.generate(() -> ThreadLocalRandom.current().nextInt(1_000_000)).limit(1_000_000)
-            .boxed().toArray(Integer[]::new);
+    SetCollectorBenchmark scb = new SetCollectorBenchmark();
 
-    @Benchmark
-    public Set<Integer> collectSet() {
-        return Arrays.stream(millionInts).collect(new CollectorToSet<>());
+    @Test
+    public void testSetCollection() {
+        final Set<Integer> set1 = scb.collectSet();
+        final Set<Integer> set2 = scb.collectConcurrentSet();
+        assertThat(set1, is(set2));
     }
 
-    @Benchmark
-    public Set<Integer> collectConcurrentSet() {
-        return Arrays.stream(millionInts).collect(new ConcurrentCollectorToSet<>());
-    }
 
 }
