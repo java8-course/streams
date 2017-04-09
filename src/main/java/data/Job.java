144b468d87774be407185972f8826e4698bc1e41
alexander <aleksandr_petukhov@epam.com>
2017-04-09 11:54:24 +0300

    Job class

---
+++ src/main/java/data/Job.java
@@ -0,0 +1,31 @@
+package data;
+
+public class Job {
+    private final Person person;
+    private final String employer;
+    private final int duration;
+    private final String position;
+
+    public Job(Person person, String employer, int duration, String position) {
+        this.person = person;
+        this.employer = employer;
+        this.duration = duration;
+        this.position = position;
+    }
+
+    public Person getPerson() {
+        return person;
+    }
+
+    public String getEmployer() {
+        return employer;
+    }
+
+    public int getDuration() {
+        return duration;
+    }
+
+    public String getPosition() {
+        return position;
+    }
+}
